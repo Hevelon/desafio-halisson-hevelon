11e4f659a3f8eec37f0866eefc3120e6a48af9b4
Hevelon <halisson.hevelon@gmail.com>
2023-08-16 23:21:24 -0300

    upload

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,6 +1,5 @@
 class CaixaDaLanchonete {
     constructor() {
-        // Definição do cardápio com informações sobre os itens e seus valores
         this.cardapio = {
             cafe: { descricao: 'Café', valor: 3.0 },
             chantily: { descricao: 'Chantily (extra do Café)', valor: 1.5 },
@@ -12,55 +11,44 @@ class CaixaDaLanchonete {
             combo2: { descricao: '1 Café e 1 Sanduíche', valor: 7.5 }
         };
     
-        // Definição das formas de pagamento aceitas
         this.formasDePagamento = ['dinheiro', 'debito', 'credito'];
     }
 
     calcularValorDaCompra(formaDePagamento, itens) {
-        // Verificação da forma de pagamento
         if (!this.formasDePagamento.includes(formaDePagamento)) {
             return "Forma de pagamento inválida!";
         }
 
-        // Objeto para contar as quantidades de itens
-        const itensQuantidades = {};
+        let valorTotal = 0;
 
-        // Iteração pelos itens da lista
+        const itensQuantidades = {};
         for (const itemInfo of itens) {
             const [codigo, quantidade] = itemInfo.split(',');
-            // Verificação se o código do item existe no cardápio
             if (!this.cardapio[codigo]) {
                 return "Item inválido!";
             }
-            // Contagem da quantidade de cada item
             itensQuantidades[codigo] = (itensQuantidades[codigo] || 0) + parseInt(quantidade);
         }
 
-        // Cálculo do valor total
-        let valorTotal = 0;
         for (const codigo in itensQuantidades) {
             const itemInfo = this.cardapio[codigo];
             const quantidade = itensQuantidades[codigo];
 
-            // Verificação de itens extras sem seus principais
             if (itemInfo.descricao.includes('extra') && !this.cardapio[codigo.replace('extra', '')]) {
                 return "Item extra não pode ser pedido sem o principal";
             }
 
-            // Cálculo do valor total do item
             valorTotal += itemInfo.valor * quantidade;
         }
 
-        // Aplicação de descontos e taxas
         if (formaDePagamento === 'dinheiro') {
-            valorTotal *= 0.95; // Desconto de 5% para pagamento em dinheiro
+            valorTotal *= 0.95; // Aplicando desconto de 5% para pagamento em dinheiro
         } else if (formaDePagamento === 'credito') {
-            valorTotal *= 1.03; // Acréscimo de 3% para pagamento em crédito
+            valorTotal *= 1.03; // Aplicando acréscimo de 3% para pagamento em crédito
         }
 
-        // Formatação da saída com "R$" e duas casas decimais
         return `R$ ${valorTotal.toFixed(2)}`;
     }
 }
 
-module.exports = CaixaDaLanchonete; // Exportação da classe
+module.exports = CaixaDaLanchonete;
